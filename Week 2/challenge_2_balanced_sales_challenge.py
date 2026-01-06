535b194a6f9552a60a5c2436881a6b7ecdac9dc4
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2026-01-07 00:11:28 +0500

    Some changes

--- Week 2/challenge_2_balanced_sales_challenge.py
+++ Week 2/challenge_2_balanced_sales_challenge.py
@@ -4,13 +4,20 @@ def can_balance_scales(arr):
     if len(arr) == 0:
         return True
 
+    total = sum(arr)
+
+    split = total / 2
+
+    if split % 2 != 0:
+        return False
+
     arr.sort()
     bagA = arr
     bagB = []
 
     for i in range(len(arr)):
         if bagB:
-            if sum(bagA) == sum(bagB):
+            if sum(bagA) == sum(bagB) == int(split):
                 return True
         temp = bagA.pop(0)
         bagB.append(temp)
