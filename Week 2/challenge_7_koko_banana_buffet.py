c1f2bbfdbbbc067bcd1dd37eeca5834a761a57d2
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2026-01-07 22:25:51 +0500

    Update challenge_7_koko_banana_buffet.py

--- Week 2/challenge_7_koko_banana_buffet.py
+++ Week 2/challenge_7_koko_banana_buffet.py
@@ -1,34 +1,30 @@
-
-def hours_check(low, high, mid, piles):
-    
-    piles = piles[low : high + 1]
-    result = piles[mid]
+ 
+def hours_check(piles, mid):
     
     count = 0
-    for i in range(len(piles)):
-        count += (piles[i] + piles[mid] - 1) // piles[mid]
-    
-    if count == result:
-        return True
-    
-    return False
+    for pile in piles:
+        count += (pile + mid - 1) // mid
+
+    return count
+
+
 
 def calculate_minimum_speed(piles, k):
 
-    low = 0
-    high = len(piles) - 1
+    low = 1
+    high = max(piles)
 
     while low < high:
+        
         mid = (low + high) // 2
-        result = hours_check(low, high, mid, piles)
-
-        if result:
+        result = hours_check(piles, mid)
+        if mid == result:
             return mid
         
-        elif result <= k:
+        elif mid <= k:
             high = mid
-        
+
         else:
-            low = mid + 1
+            low = mid - 1
 
     return mid
