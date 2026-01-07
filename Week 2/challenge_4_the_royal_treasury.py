e83313dd2d5ebc5898a7c8ddf34c41b4913acce2
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2026-01-07 23:19:32 +0500

    Some changes

--- Week 2/challenge_4_the_royal_treasury.py
+++ Week 2/challenge_4_the_royal_treasury.py
@@ -1,24 +1,40 @@
 
+# def count_payment_combinations(coins, total_sum):
+
+#     possibilities = {}
+
+#     def counting_ways(current, add):
+#         if add == total_sum:
+#             return 1
+        
+#         if add > total_sum:
+#             return 0
+        
+#         if current == len(coins):
+#             return 0
+
+#         if (current, add) in possibilities:
+#             return possibilities[(current, add)]
+        
+#         possibilities[(current, add)] = counting_ways(current, add + coins[current]) + counting_ways(current + 1, add)
+#         return possibilities[(current, add)]
+    
+#     return counting_ways(0, 0)
+
+# print(count_payment_combinations([1, 2], 4))
+
+
 def count_payment_combinations(coins, total_sum):
 
-    possibilities = {}
+    possibilities = [0] * (total_sum + 1)
 
-    def counting_ways(current, add):
-        if add == total_sum:
-            return 1
-        
-        if add > total_sum:
-            return 0
-        
-        if current == len(coins):
-            return 0
+    possibilities[0] = 1
 
-        if (current, add) in possibilities:
-            return possibilities[(current, add)]
-        
-        possibilities[(current, add)] = counting_ways(current, add + coins[current]) + counting_ways(current + 1, add)
-        return possibilities[(current, add)]
+    for coin in coins:
+        for i in range(coin, total_sum + 1):
+            possibilities[i] += possibilities[i - coin]
     
-    return counting_ways(0, 0)
+
+    return possibilities[total_sum]
 
 print(count_payment_combinations([1, 2], 4))
