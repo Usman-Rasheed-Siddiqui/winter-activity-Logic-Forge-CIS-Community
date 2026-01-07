85946e1cb2ce2806c7f9aa9815669d6bc3fbddfd
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2026-01-07 09:23:18 +0500

    Whole challenge two code change for different even and odd case

--- Week 2/challenge_3_mirror_quest.py
+++ Week 2/challenge_3_mirror_quest.py
@@ -18,3 +18,34 @@ def find_longest_mirror_length(s):
             return 2 + find_longest_mirror_length(s[i + 1: ])
 
 print(find_longest_mirror_length("bbabcbcab"))
+
+
+def find_longest_mirror_length(s, index):
+
+    length = len(s)
+    longest_length = 0
+
+    # odd length
+
+    for i in range(length):
+        left, right = i, i
+        while left <= 0 and right < length and s[left] == s[right]:
+            temp_length = (right - left) + 1
+            if temp_length > longest_length:
+                longest_length = temp_length
+            
+            left -= 1
+            right += 1
+
+    for  i in range(length):
+        left = i
+        right = i + 1
+        while left <= 0 and right < length and s[left] == s[right]:
+            temp_length = (right - left) + 1
+            if temp_length > longest_length:
+                longest_length = temp_length
+            
+            left -= 1
+            right += 1
+    
+    return longest_length
