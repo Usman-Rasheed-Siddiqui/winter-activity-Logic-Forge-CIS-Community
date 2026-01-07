c5806f1a4571dbc9345a338b73c45864159fc01c
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2026-01-07 09:23:59 +0500

    Update challenge_3_mirror_quest.py

--- Week 2/challenge_3_mirror_quest.py
+++ Week 2/challenge_3_mirror_quest.py
@@ -2,27 +2,6 @@
 def find_longest_mirror_length(s):
 
     length = len(s)
-
-    if length == 1:
-        return 1
-
-    i = 0
-    j = len(s) - 1
-
-    while i < j:
-        if s[i] != s[j]:
-            max_string = max(s[i + 1:], s[i: j])
-            return find_longest_mirror_length(max_string)
-        
-        else:
-            return 2 + find_longest_mirror_length(s[i + 1: ])
-
-print(find_longest_mirror_length("bbabcbcab"))
-
-
-def find_longest_mirror_length(s, index):
-
-    length = len(s)
     longest_length = 0
 
     # odd length
@@ -49,3 +28,5 @@ def find_longest_mirror_length(s, index):
             right += 1
     
     return longest_length
+
+print(find_longest_mirror_length("bbabcbcab"))
