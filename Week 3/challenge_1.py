a9091e89608ddd317e50b7cf71d5aea5c4c8bf96
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2026-01-13 23:52:05 +0500

    Update challenge_1.py

--- Week 3/challenge_1.py
+++ Week 3/challenge_1.py
@@ -13,8 +13,41 @@ def smart_city_temperature_alert(K, temp):
             j += 1
     
     print(alerts)
+    return alerts
 
+def next_alert(alerts):
+
+    NEXT = int(input("Enter Temperature Index for Alert: "))
+    print("NEXT", NEXT)
+
+    if alerts[NEXT]:
+        return alerts[NEXT]
+
+    else:
+        return "No Alert"
+
+def count_alerts(alerts):
+    COUNT = int(input("Enter Initial and Final Count Indices with space: ")).split()
+    number = 0
+
+    for alert in alerts:
+        if alert:
+            number += 1
+    
+
+    return number
 
 K = 3
 temp = [73, 74, 75, 71, 69, 72, 76, 73]
-smart_city_temperature_alert(K, temp)
+
+alerts = smart_city_temperature_alert(K, temp)
+
+print(next_alert(alerts))
+print(next_alert(alerts))
+
+print(count_alerts(alerts))
+print(count_alerts(alerts))
+
+COUNT = []
+stop = False
+
